dbc9fe9ae93fb4de063b2c82a9bf29cd3ec41969
Tajetti <talhettialvaro@gmail.comm>
2025-04-15 20:52:09 -0300

    Manipulando o valor do input

---
+++ script.js
@@ -0,0 +1,7 @@
+const amount = document.getElementById("amount");
+
+// Manipulando o input amount para receber somente números.
+amount.addEventListener("input", (event) => {
+  console.log(amount.value)
+})
+

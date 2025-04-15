595abc8adb662ca015e19fc88304e62f4de99376
Tajetti <talhettialvaro@gmail.comm>
2025-04-15 20:56:28 -0300

    Validando para apenas números

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 const amount = document.getElementById("amount");
 
 // Manipulando o input amount para receber somente números.
-amount.addEventListener("input", (event) => {
-  console.log(amount.value)
+amount.addEventListener("input", (event) => {  
+  const hasCaractersRegex = /\D+/g 
+  // Regex para encontrar caracteres que não sejam números
+  
+  amount.value = amount.value.replace(hasCaractersRegex, ""); 
+  // Substitui os caracteres não numéricos por vazio
 })
 

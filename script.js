cb25036f3e04ff8871c2e466695099335132d1e6
Tajetti <talhettialvaro@gmail.comm>
2025-04-15 21:05:54 -0300

    Recuperando o tipo da moeda

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
+//Obtendos os elementos do formulário.
+const form = document.querySelector("form"); 
 const amount = document.getElementById("amount");
+const currency = document.getElementById("currency");
 
 // Manipulando o input amount para receber somente números.
 amount.addEventListener("input", (event) => {  
@@ -8,4 +11,10 @@ amount.addEventListener("input", (event) => {
   amount.value = amount.value.replace(hasCaractersRegex, ""); 
   // Substitui os caracteres não numéricos por vazio
 })
+
+// Capturando o evento de submit (enviar)  do formulário.
+form.onsubmit = (event) => {
+  event.preventDefault(); // Previne o comportamento padrão do formulário (recarregar a página)
+  console.log(currency.value)
+}
 
